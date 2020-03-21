f59f8abfd76f4f22b9afc25afb72b358eb14ca55
Prakhar <prakharjagwani7@gmail.com>
2020-03-21 19:13:50 +0530

    Login system added

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm 
 # Create your views here.
 def register(request):
@@ -10,10 +11,14 @@ def register(request):
             username = form.cleaned_data.get('username')
             print(username)
             messages.success(request,f'Account created for {username}')
-            return redirect('home')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
     return render(request,'users/register.html',{'title':'Register','form': form})
 
 def homeView(request):
     return render(request,'users/homepage.html',{'title':'Home'})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html',{'title':'User Profile'})
