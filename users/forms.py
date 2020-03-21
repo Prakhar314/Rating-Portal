ea0f414731467ea1b442f2bdd8ccdcb6d00dcd71
Prakhar <prakharjagwani7@gmail.com>
2020-03-21 18:36:03 +0530

    Basic Validation

--- users/forms.py
+++ users/forms.py
@@ -8,3 +8,9 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields=['username','email','password1','password2']
+
+    def clean_email(self,*args,**kwargs):
+        email =self.cleaned_data.get("email")
+        if not email.endswith("iitd.ac.in"):
+            raise forms.ValidationError("Use a valid IIT-D email")
+        return email
